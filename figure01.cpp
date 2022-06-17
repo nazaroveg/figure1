335684c7aa671c0701de62e3dfd46a954ebc59a7
nazaroveg <nazaroveg@yandex.ru>
2022-06-17 12:12:23 +0300

    3 спасибо

--- figure01.cpp
+++ figure01.cpp
@@ -2,25 +2,29 @@
 //nazaroveg@yandex.ru
 
 #include <iostream>
+#include <string>
 
 class Figure
 {
 public:
+	Figure(){}
 	
-	
-
-	void get_sides_count()
+	int get_sides_count() const
 	{
-		std::cout << "Фигура: " << sides_count << std::endl;
+		return sides_count;
 	}
-
+	std::string get_name() const
+	{
+		return name;
+	}
+	
 private:
 	
 
 
 protected:
 	int sides_count = 0;
-	
+	std::string name = { "Фигура" };
 
 };
 
@@ -29,20 +33,20 @@ protected:
 class Triangle : protected Figure
 {
 public:
+	
 	Triangle()
 	{
-	Figure::sides_count = 3;
+	sides_count = 3;
+	name = "Треугольник";	 
 	}
-	
-	
-	
-	void sides_count()
+	int get_sides_count() const
 	{
-		std::cout << "Треугольник: " << Figure::sides_count << std::endl;
+		return sides_count;
 	}
-
-	
-
+	std::string get_name() const
+	{
+		return name;
+	}
 };
 
 
@@ -51,16 +55,17 @@ class Quadrangle : protected Figure
 public:
 	Quadrangle()
 	{
-		Figure::sides_count = 4;
+	sides_count = 4;
+	name = "Четырёхугольник";
 	}
-	
-	void sides_count()
+	int get_sides_count() const
 	{
-		
-		std::cout << "Четырёхугольник: " << Figure::sides_count << std::endl;
+		return sides_count;
 	}
-	
-
+	std::string get_name() const
+	{
+		return name;
+	}
 };
 
 
@@ -73,9 +78,11 @@ int main()
 	Quadrangle qu{};
 	std::cout << "Количество сторон: " << std::endl;
 
-	fi.get_sides_count();
-	tr.sides_count();
-	qu.sides_count();
+	std::cout << fi.get_name() << ": " << fi.get_sides_count() << "\n";
+	std::cout << tr.get_name() << ": " << tr.get_sides_count() << "\n";
+	std::cout << qu.get_name() << ": " << qu.get_sides_count() << "\n";
+	
+	
 	
 	
 	
